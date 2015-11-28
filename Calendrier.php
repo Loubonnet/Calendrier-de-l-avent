f6f619599bf8c18c79ac1637aa783fb765681e6c
Bonnet Louise <louise.bonnet@etu.utc.fr>
2015-11-28 19:50:56 +0100

    Pizza Time

--- Calendrier.php
+++ Calendrier.php
@@ -5,9 +5,12 @@
         <link rel="stylesheet" type="text/css" href="calendrier.css">
         
 	</head>
+    <script src="js/jquery-1.10.2.min.js"></script> 
+    <script src="js/lightbox-2.6.min.js"></script> 
+    <link href="css/lightbox.css" rel="stylesheet" />
     <?php
-        $poshorizontal=array(510,310,600,1000,250,780,100,10,850,700,520,160,340,30,110,1130,800,1080,30,500,180,670,1010,990,290);
-        $posvertical=array(455,350,120,580,25,15,420,10,480,300,20,140,170,540,250,500,200,40,180,250,560,500,390,210,480);
+        $poshorizontal=array(510,310,600,1000,280,780,100,10,850,760,520,160,380,30,110,1130,800,1080,30,480,180,670,1010,990,320);
+        $posvertical=array(455,350,120,580,25,15,420,10,480,300,20,110,170,540,250,500,180,40,180,250,560,500,390,210,480);
     ?>
 	<body>
 		<header id="titre" width="100%" height=50px>
@@ -20,7 +23,10 @@
                 for ($i=0;$i<25;$i++) {
                     $x = $poshorizontal[$i];
                     $y = $posvertical[$i];
-                    echo '<li id="normal" style="margin-left:'.$x.'px;margin-top:'.$y.'px">'.'salut'.($i+1).'</li>';
+                    if (($i + 1) % 5 !=0)
+                        echo '<li id="normal" style="margin-left:'.$x.'px;margin-top:'.$y.'px"><a data-lightbox="image'.($i+1).'" href="img/image'.($i+1).'.jpg"><img src="img/tn_image'.($i+1).'.jpg" alt=""></a></li>';
+                    else 
+                        echo '<li id="kingsize" style="margin-left:'.$x.'px;margin-top:'.$y.'px"><a data-lightbox="image'.($i+1).'" href="img/image'.($i+1).'.jpg"><img src="img/tn_image'.($i+1).'.jpg" alt=""></a></li>';
                     }
                 echo '</ol>';
             ?>
